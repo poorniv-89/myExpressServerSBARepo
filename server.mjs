e3f78ee5eb69fec848edb49dbd015db8ab54b13e
poorniv-89 <poorniv.89@gmail.com>
2025-04-20 17:28:06 -0600

    added HATEOAS link to recipe route

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ import recipeRoutes from './routes/recipeRoutes.mjs';
 import commentRoutes from './routes/commentRoutes.mjs'
 import userRoutes from './routes/userRoutes.mjs';
 import { logger } from './middleware/logger.mjs';
-import {errorHandler} from './middleware/errorHandler.mjs';
+import { errorHandler } from './middleware/errorHandler.mjs';
 
 //setups
 const app = express();
@@ -16,24 +16,55 @@ app.use(express.static('public'));
 
 //middleware 
 app.use(logger);
+
+//routes
 app.use('/recipes', recipeRoutes);
 app.use('/comments', commentRoutes);
 app.use('/users', userRoutes);
 
+// Adding some HATEOAS links for recipe route .
+app.get('/', (req, res) => {
+    res.json({
+        links: [
+            {
+                href: '/recipes',
+                rel: 'recipes',
+                method: 'GET',
+            },
+            {
+                href: '/recipes/',
+                rel: 'create-new-recipe',
+                method: 'POST',
+                body: {
+                    userId: 'string',
+                    recipename: 'string',
+                    ingredients: ['string'],
+                    recipe: 'string'
+                }
+            },
+            {
+                href: '/recipes/{id}',
+                rel: 'get-recipe-by-id',
+                method: 'GET'
+            }
+        ]
+    });
+});
+
 
 //error handling middleware
-
 app.use(errorHandler);
 
-//test route for server
 app.get('/recipes', (req, res) => {
     res.render('recipes');
-  });
-  app.get('/', (req, res)=>{
+});
+
+//test route for server
+app.get('/', (req, res) => {
     res.send('Server is sending a response');
 })
 
 //listener
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on PORT: ${PORT}`);
 })
